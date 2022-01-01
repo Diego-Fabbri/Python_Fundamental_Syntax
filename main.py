a12740f789b7e797a2dc0d56f973d34cb29f4a4b
Diego Fabbri <diegofabbri96@gmail.com>
2022-01-01 19:34:13 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -300,3 +300,180 @@ else:
 
 
 ### LOOPS: FOR (11)
+
+list_of_cities = ["Milan","Rome","Cosenza"] # list
+string = "letter" #using strings
+
+for city in list_of_cities:
+    print(city)
+
+for c in string:
+    print(c)
+
+for x in range(6):  # using range()
+    print(x)
+
+# else: it works at the end of for loop
+for x in range(6):  
+    print(x)
+else:
+    print("end of for loop")
+
+
+# break: it stops for loop iterations
+for x in range(6):  
+    if x==3:
+        print("For loop iterations stopped")
+        break      
+    print(x)
+
+# continue: it skips for loop iterations
+for x in range(6):  
+    if x == 3:
+        print("For loop iteration skipped")
+        continue      
+    print(x)
+
+
+### COLLECTION OF DATA: list, tuple, set, dictionary (12- Theory)
+
+## Characteristics of a data collection: 
+# - ordered: elements have a defined order
+# - indexing: elements are accessible by an index
+# - editable: elements can be modified, removed or added 
+# - immutable: elements cannot be modified, removed or added 
+# - allow duplicates: elements can be duplicated 
+
+# Important: if elements are ordered you can access them by an index
+
+## Data collection definitions
+# - Lists are ordered and editable data collection. Lists allow duplicates
+# - Tuples are ordered and immutable data collection. Tuple allow duplicates
+
+# - Sets are unordered and unindexed data collection. Sets do not allow duplicates. (elements cannot be modified)
+# - Dictionaries are ordered and editable data collection (version 3.7). Dictionaries do not allow duplicates.
+
+### DATA COLLECTION: LIST (13)
+# List is a ordered, indexed, editable and duplicates allowing data collection.
+
+x = ["Milan","Rome","Cosenza","Turin"] # same type (Version 1)
+type(x) # get type of the list
+len(x) # get length of the list
+
+# x = list("Milan","Rome","Cosenza") # (Version 2)
+
+y = ["Hello", 95, False] # different type
+
+print(x[0]) # access element of the list
+print(x[-1]) # acces last element
+print(x[2:]) # access elements by a range (from element in position 2 to last element)
+print(x[1:3]) # access elemets by a range (from element in position 1 to element in position 3-1)
+print(x[:2]) # access elements by a range (from element in position 0 to element in position 2-1)
+
+# WARNING: you can also use negative index
+
+## access elements
+
+x[0] = "New York" # replace a single value
+print(x)
+
+x[1:3] = ["Boston","Paris"] # replace values by a range
+print(x)
+
+x[1:3] = ["Miami"] # replace multiple values with only one
+print(x)
+print(len(x))
+
+# add elements
+
+x.append("Berlin") # insert the element at the end of the list
+print(x)
+print(len(x))
+
+x.insert(1, "Florence") # insert element in position 1
+print(x)
+print(len(x))
+
+y = ["Barcelona", "Budapest"]
+
+x.extend(y) # add elements of y list to the end of x
+print(x)
+print(len(x))
+
+# remove elements
+
+x.remove("Barcelona") # remove an element according to its value
+print(x)
+
+x.pop() # remove the last element of a list
+print(x)
+
+x.pop(1) # remove element in position 1
+print(x)
+
+del x[0] # remove element in position 0 of the list (you can also remove the entire list using del x)
+print(x)
+
+x.clear() # remove all elemets from the list making it empty
+print(x)
+
+x = ['New York', 'Florence', 'Miami', 'Turin', 'Berlin', 'Barcelona', 'Budapest']
+
+# loops on elements
+
+for city in x:
+    print(city)
+
+for i in range(len(x)):
+    print(x[i])
+
+i = 0
+while i < len(x):
+    print(x[i])
+    i = i + 1
+
+# list comprehension: allow to use a shorter syntax
+
+[print(city) for city in x if city != "Turin"]
+
+# edit order of a list
+x = ['New York', 'Florence', 'Miami', 'Turin', 'Berlin', 'Barcelona', 'Budapest']
+
+x.sort() # sorts elements in numerical/ alphabetical order (ascending)
+x.sort(reverse = True) # sorts elements in reversed numerical/ alphabetical order (descending)
+
+# how to copy a list
+x = ['New York', 'Florence', 'Miami', 'Turin', 'Berlin', 'Barcelona', 'Budapest']
+y = x
+
+y[0] = "Athens" # if you change values in y, corresponding ones change in x due to  y=x
+                # to avoid that you need a copy
+
+print(x)
+
+y = x.copy() # or use   y = list(x)
+print(y)
+y[0] = "New York"
+print(y)
+print(x) # using copy() the source list x does not change
+
+# how to make union of lists: +, append(), extend()
+x = ['New York', 'Florence', 'Miami', 'Turin', 'Berlin', 'Barcelona', 'Budapest']
+y = ["Athens", "Beijing"]
+
+print(x + y) #using +
+
+for city in y: # using append()
+    x.append(city)
+else:
+    print(x)
+
+x.extend(y) # using extend()
+
+### DATA COLLECTION: TUPLE (14)
+# Tuple is a ordered, indexed, uneditable and duplicates allowing data collection.
+
+
+
+
+
